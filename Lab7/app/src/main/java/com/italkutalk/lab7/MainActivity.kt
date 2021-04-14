3265c2fef9417c03d0e7d000eeae9f3e95b497b0
B07170035 <leo81407078@gmail.com>
2021-04-14 11:28:21 +0800

    B07170035

--- Lab7/app/src/main/java/com/italkutalk/lab7/MainActivity.kt
+++ Lab7/app/src/main/java/com/italkutalk/lab7/MainActivity.kt
@@ -9,8 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //將變數與 XML 元件綁定
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        val gridView = findViewById<GridView>(R.id.gridView)
+        //val spinner = findViewById<Spinner>(R.id.spinner)
+        //val gridView = findViewById<GridView>(R.id.gridView)
         val listView = findViewById<ListView>(R.id.listView)
         val count = ArrayList<String>() //儲存購買數量資訊
         val item = ArrayList<Item>() //儲存水果資訊
@@ -19,19 +19,19 @@ class MainActivity : AppCompatActivity() {
             resources.obtainTypedArray(R.array.image_list) //從 R 類別讀取圖檔
         for(i in 0 until array.length()) {
             val photo = array.getResourceId(i,0) //水果圖片 Id
-            val name = "水果${i+1}" //水果名稱
+            val name = "水果${i+1}\nPrice" //水果名稱
             val price = priceRange.random() //亂數產生價格
             count.add("${i+1}個") //新增可購買數量資訊
             item.add(Item(photo, name, price)) //新增水果資訊
         }
         array.recycle() //釋放圖檔資源
         //建立 ArrayAdapter 物件，並傳入字串與 simple_list_item_1.xml
-        spinner.adapter = ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_1, count)
+        //spinner.adapter = ArrayAdapter<String>(this,
+            //android.R.layout.simple_list_item_1, count)
         //設定橫向顯示列數
-        gridView.numColumns = 3
+        //gridView.numColumns = 3
         //建立 MyAdapter 物件，並傳入 adapter_vertical 作為畫面
-        gridView.adapter = MyAdapter(this, item, R.layout.adapter_vertical)
+        //gridView.adapter = MyAdapter(this, item, R.layout.adapter_vertical)
         //建立 MyAdapter 物件，並傳入 adapter_horizontal 作為畫面
         listView.adapter = MyAdapter(this, item,
             R.layout.adapter_horizontal)
